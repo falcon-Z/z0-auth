4119f85b8b478e47797edf248c246ee226f34188
sai <saimadhan@outlook.in>
2025-07-06 13:55:29 +0530

    Refactor renderer function for improved readability and maintainability

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,16 +1,22 @@
 import { jsxRenderer } from "hono/jsx-renderer";
 import { Link, ViteClient } from "vite-ssr-components/hono";
 
-export const renderer = jsxRenderer(({ children }) => {
-  return (
-    <html class={"dark"}>
-      <head>
-        <ViteClient />
-        <Link href="/src/index.css" rel="stylesheet" />
-      </head>
-      <body>
-        <main class={"h-screen w-full"}>{children}</main>
-      </body>
-    </html>
-  );
-});
+export const renderer = jsxRenderer(
+  ({ children }) => {
+    return (
+      <html class={"dark"}>
+        <head>
+          <ViteClient />
+          <Link href="/src/index.css" rel="stylesheet" />
+        </head>
+        <body>
+          <main class={"h-screen w-full"}>{children}</main>
+        </body>
+      </html>
+    );
+  },
+  {
+    docType: true,
+    stream: false,
+  }
+);
