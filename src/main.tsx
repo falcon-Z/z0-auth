070fa21781ddf3644e4cd6c7fb8dfe2bbeea4a1b
sai <saimadhan@outlook.in>
2025-07-31 17:49:53 +0530

    refactor: remove unused CSS styles and update main entry point to use RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -7,20 +7,19 @@
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./app/app";
+import { RouterProvider } from "react-router";
+import { router } from "./app/routes";
 
 const elem = document.getElementById("root")!;
 const app = (
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
 
 if (import.meta.hot) {
-  // With hot module reloading, `import.meta.hot.data` is persisted.
   const root = (import.meta.hot.data.root ??= createRoot(elem));
   root.render(app);
 } else {
-  // The hot module reloading API is not available in production.
   createRoot(elem).render(app);
 }
